56311520527ef791caa8c94994a33f24a89ef6fc
Giuliana Cabral <cabral.giu@hotmail.com>
2022-08-11 16:53:59 -0300

    LAB Javascript Functions & Arrays

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,64 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
 
-
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(wordArray) {
+  if (!wordArray.lenth) {
+    return null;
+  }
+
+  let longestWord = ""
+
+  for (let i = 0; i < wordArray.length; i++) {
+    if (wordArray[i].length > longestWord.length) {
+      longestWord = wordArray[i]
+    }
+  }
+
+  return longestWord;
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numberArray) {
+  return sum(numberArray);
+
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+const mixedArr = [2, 4, 6, true, "São Paulo", "avião", "cachorro"]
+
+function sum(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "object") {
+      throw new Error("Unsupported data type sir or ma'am");
+    }
+
+    if (typeof arr[i] === "string") {
+      sum += arr[i].length;
+    } else {
+      sum += arr[i];
+    }
+  }
+
+  return sum;
+}
 
 
 
@@ -26,13 +66,37 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numberArr) {
+  return avg(numberArr);
+}
+
+function avg(arr) {
+  if (!arr.length) {
+    return null;
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+      sum += arr[i].length;
+    } else {
+      sum += arr[i];
+    }
+  }
+
+  const avg = sum / arr.length;
+
+  return parseFloat(avg.toFixed(2));
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(words) {
+  return avg(words);
+ }
 
 // Bonus - Iteration #4.1
 function avg() {}
@@ -52,14 +116,45 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(words) {
+  if (!words.length) {
+    return null;
+  }
+
+  const uniqueWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const index = uniqueWords.indexOf(words[i]);
+
+    if (index < 0) {
+      uniqueWords.push(words[i]);
+    }
+  }
+
+  return uniqueWords;
+}
 
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(wordArr, searchTerm) {
+  if (!wordArr.length) {
+    return null;
+  }
+
+  let found = false;
+
+  for (let i = 0; i < wordArr.length; i++) {
+    if (wordArr[i] === searchTerm) {
+      found = true;
+    }
+  }
+
+  return found;
+}
+
 
 
 
@@ -78,7 +173,21 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(wordsArr, searchTerm) {
+  if (!wordsArr.length) {
+    return 0;
+  }
+
+  let count = 0;
+
+  for (let i = 0; i < wordsArr.length; i++) {
+    if (searchTerm === wordsArr[i]) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 
 
@@ -106,7 +215,42 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct(matrix) {
+  let product = 1;
+
+  const numberOfRows = matrix.length;
+  const numberOfColumns = matrix[0].length;
+
+  for (let row = 0; row < numberOfRows; row++) {
+    for (let column = 0; column < numberOfColumns - 3; column++) {
+      let rowProduct =
+        matrix[row][column] *
+        matrix[row][column + 1] *
+        matrix[row][column + 2] *
+        matrix[row][column + 3];
+
+      if (rowProduct > product) {
+        product = rowProduct;
+      }
+    }
+  }
+
+  for (let column = 0; column < numberOfColumns; column++) {
+    for (let row = 0; row < numberOfRows - 3; row++) {
+      let columnProduct =
+        matrix[row][column] *
+        matrix[row + 1][column] *
+        matrix[row + 2][column] *
+        matrix[row + 3][column];
+
+      if (columnProduct > product) {
+        product = columnProduct;
+      }
+    }
+  }
+
+  return product;
+}
 
 
 
